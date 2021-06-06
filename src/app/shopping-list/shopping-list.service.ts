67c1026aa5d671bf60abfd20705b12c0ae6b4998
7774j <80786885+7774j@users.noreply.github.com>
2021-06-05 20:41:39 -0500

    Work on recipe project

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -10,9 +10,16 @@ export class ShoppingListService {
     new Ingredient ('tomatoes', 10),
 
   ];
+
   getIngredients(){
     return this.ingredients.slice();
   }
+
+  getIngredient(index: number){
+    return this.ingredients[index];
+
+  }
+
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
     this.ingredientsChanged.next(this.ingredients.slice());
@@ -21,5 +28,14 @@ export class ShoppingListService {
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
 
+  }
+  updateIngredient(index: number, newIngredient: Ingredient){
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+
+  }
+  deleteIngredient(index: number){
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
