16910bab9ab6801dbda535acba5edfb9248c9d6e
7774j <80786885+7774j@users.noreply.github.com>
2021-06-09 16:17:00 -0500

    Coursework due June 7th
    Today June 9th, but finished

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,24 +8,31 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Test Recipe',
-    'This is a test',
-     'https://c.pxhere.com/photos/c8/ea/appetizer_bowl_delicious_dish_epicure_food_fries_guacamole-1557385.jpg!d',
-     [
-       new Ingredient('Meat', 1),
-       new Ingredient('tomatoes', 20)
-     ]),
-    new Recipe('Another Test Recipe',
-    'This is simply a test',
-    'https://c.pxhere.com/photos/c8/ea/appetizer_bowl_delicious_dish_epicure_food_fries_guacamole-1557385.jpg!d',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('Parsnips', 45)
-    ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Test Recipe',
+  //   'This is a test',
+  //    'https://c.pxhere.com/photos/c8/ea/appetizer_bowl_delicious_dish_epicure_food_fries_guacamole-1557385.jpg!d',
+  //    [
+  //      new Ingredient('Meat', 1),
+  //      new Ingredient('tomatoes', 20)
+  //    ]),
+  //   new Recipe('Another Test Recipe',
+  //   'This is simply a test',
+  //   'https://c.pxhere.com/photos/c8/ea/appetizer_bowl_delicious_dish_epicure_food_fries_guacamole-1557385.jpg!d',
+  //   [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('Parsnips', 45)
+  //   ])
+  // ];
+
+  private recipes: Recipe[] = [];
+
   constructor(private slService: ShoppingListService){
 
+  }
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice())
   }
 
   getRecipes(){
