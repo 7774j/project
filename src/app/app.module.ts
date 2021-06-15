012985d262259713b5ed0cb291caa7e185a07cc7
Lee Hodges <leehodges@gmail.com>
2021-06-15 16:16:25 -0500

    fixes
    * removed debugger from recipes.component.html debugger statements can only be used in .ts files in a function.
    
    * The app-recipe-list & router-outlet not a known element errors in terminal let me know that those components are not accessible to the recipes.componet
    
    * The first place to check is app.module.ts to make sure our recipecomponent is declared
    
    * In this case it was not, and once declared/imported everything seems to be working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import {RecipesComponent} from './recipes/recipes.component';
 
 
 
@@ -40,6 +41,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     RecipeEditComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipesComponent
 
 
 
