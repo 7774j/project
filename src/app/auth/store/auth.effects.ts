2c2fbea1544eae0e03ac2f25605edd4d97b993f9
7774j <80786885+7774j@users.noreply.github.com>
2021-06-23 16:34:10 -0500

    Work with ngrx

---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,53 @@
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+import { of } from "rxjs";
+import { catchError, switchMap, map } from "rxjs/operators";
+import * as AuthActions from "./auth.actions";
+
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+export class AuthEffects{
+    // authLogin = this.actions$.pipe(
+    //   ofType(AuthActions.LOGIN_START),
+    //   switchMap((authData: AuthActions.LoginStart) => {
+    //     return this.http.post<AuthResponseData>(
+    //       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
+    //     {
+    //       email: authData.payload.email,
+    //       password: authData.payload.password,
+    //       returnSecureToken: true
+    //     }
+    //     )
+    //   })
+    // );
+    @Effect()
+      authLogin = this.actions$.pipe(
+        ofType(AuthActions.LOGIN_START),
+        switchMap((authData: AuthActions.LoginStart) => {
+        return this.http.post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
+              {
+                email: authData.payload.email,
+                password: authData.payload.password,
+                returnSecureToken: true
+              }
+              ).pipe(catchError(error => {
+                of();
+              }), map(resData => {
+                of();
+              })
+              );
+            }),
+
+    )
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
